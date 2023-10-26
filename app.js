627f6150e3f8d1e55ddc989fd387d34bdbee482b
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-10-26 16:46:09 +0400

    Added campground model basic setup

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground')
+
+mongoose.connect('mongodb://127.0.0.1:27017/Wilderness-Journey', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database connected');
+});
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello from the wild')
-})
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+// app.get('/', (req, res) => {
+   // res.render('home')
+// })
+
+app.get('/makecampground', async (req, res) => {
+    try {
+        const camp = new Campground({ title: 'My backyard', description: 'cheap camping' });
+        await camp.save();
+        res.send(camp);
+    } catch (error) {
+        console.error('Error:', error);
+        res.status(500).send('Error occurred while creating campground');
+    }
+});
 
 
 app.listen(3000, () => {
