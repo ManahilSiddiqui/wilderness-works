ee0c82e457d97031158fc7961ec7552ae315a5a7
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-10-31 12:07:51 +0400

    Campground New and create

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ mongoose.connect('mongodb://127.0.0.1:27017/Wilderness-Journey', {
     useUnifiedTopology: true,
 });
 
-
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
@@ -17,25 +16,37 @@ db.once('open', () => {
 
 const app = express();
 
-
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
+
+// Body parser middleware
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-   res.render('home')
+   res.render('home');
  });
 
 app.get('/campgrounds', async (req, res) => {
     const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', {campgrounds})
-})
+    res.render('campgrounds/index', { campgrounds });
+});
 
-app.get('/campgrounds/:id', async(req, res) => {
-    const campground = await Campground.findById(req.params.id)
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`); // Corrected "camgrounds" to "campgrounds"
+ });
+ 
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
     res.render('campgrounds/show', { campground });
-})
-
+});
 
 app.listen(3000, () => {
-    console.log('serving on port 3000')
-})
+    console.log('Server is listening on port 3000');
+});
