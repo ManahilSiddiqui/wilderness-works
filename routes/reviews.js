3b46b27bec667a0d3904cb319dfb60c0f5c70805
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-11-16 12:28:34 +0400

    Restructured routes

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,13 +3,13 @@ const router = express.Router({ mergeParams: true });
 const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 const Campground = require('../models/campground')
 const Review = require('../models/review');
-const Reviews = require('../controllers/reviews');
+const reviews = require('../controllers/reviews');
 const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
 
 
-router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview));
+router.post('/', isLoggedIn, validateReview, catchAsync(reviews.createReview))
 
-router.delete('/:reviewid', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview));
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(reviews.deleteReview))
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -6,15 +6,16 @@ const User = require('../models/user');
 const users = require('../controllers/users');
 const { storeReturnTo } = require('../middleware');
 
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.get('/register', users.renderRegister);
+    router.route('/login')
+    .get(users.renderLogin)
+    .post(storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login)
 
-router.post('/register', catchAsync(users.register));
+// router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
 
-router.get('/login', users.renderLogin)
-
-router.post('/login', storeReturnTo, passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), users.login);
-
-router.get('/logout', users.logout); 
+router.get('/logout', users.logout)
 
 module.exports = router;
