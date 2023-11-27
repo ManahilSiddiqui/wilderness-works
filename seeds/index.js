3441099f58eb58c34bdecf56e940cda377593461
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-11-27 18:06:14 +0400

    Reseeded database

--- seeds/index.js
+++ seeds/index.js
@@ -30,7 +30,10 @@ const seedDB = async () => {
         price,
         geometry: {
             type: 'Point',
-            coordinates: [ -98.8223185136653, 31.8039734986915 ]
+            coordinates:[ cities[random1000].longitude,
+                          cities[random1000].latitude,
+                        ]
+
         },
         images: [
             {
