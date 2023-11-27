9400118e439388b6e070f648834f58191abe2b8c
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-11-27 17:26:36 +0400

    Fixed map error

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,20 +1,20 @@
-const campground = require("../../models/campground");
+//const campground = require("../../models/campground");
 
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/light-v10', // style URL
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
     center: campground.geometry.coordinates, // starting position [lng, lat]
-    zoom: 9, // starting zoom
+    zoom: 10 // starting zoom
 });
 
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
     .setPopup(
-        new mapboxgl.Popup({offset: 25})
-           .setHTML(
-               `<h3>${campground.title}</h3><p>${campground.location}</p>`
-        )
+        new mapboxgl.Popup({ offset: 25 })
+            .setHTML(
+                `<h3>${campground.title}</h3><p>${campground.location}</p>`
+            )
     )
     .addTo(map)
 
