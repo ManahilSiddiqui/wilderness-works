77443efdad6c52d53f10522cf9a2be1dbf72a2a4
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-12-01 21:50:15 +0400

    Added a SQL injection

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 
+const mongoSanitize = require('express-mongo-sanitize');
+
 const usersRoutes = require('./routes/user');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
@@ -42,6 +44,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(mongoSanitize())
 
 const sessionConfig = {
     secret: 'thisshouldbebetter!',
