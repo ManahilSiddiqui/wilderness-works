155ffde4ca60ffe7713244d5c516cd7891d32f1e
ManahilSiddiqui <manahil_siddiqui@hotmail.com>
2023-12-03 00:22:43 +0400

    fixed error

--- seeds/index.js
+++ seeds/index.js
@@ -3,10 +3,15 @@ const cities = require('./cities');
 const {places, descriptors} = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-mongoose.connect('mongodb://127.0.0.1:27017/Wilderness-Journey', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+const dbURL = process.env.DB_URL;
+// mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+main().catch(err => console.log(err));
+
+async function main() {
+    await mongoose.connect(dbURL);
+    console.log("MONGO CONNECTION OPEN");
+    // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+};
 
 
 const db = mongoose.connection;
